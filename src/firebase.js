0bba53f1f4dc0a83b95755793af8ef55f1cc067e
Rustem Akhmetov <rustem.akhmetov@sonicdrivein.com>
2021-03-16 13:15:02 +0100

    profile updated with change name

--- src/firebase.js
+++ src/firebase.js
@@ -1,63 +1,61 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyDuVJBwLc6kIS8ri_OZLB6qrcV_DdK7pR0",
-    authDomain: "live-notes-49852.firebaseapp.com",
-    projectId: "live-notes-49852",
-    storageBucket: "live-notes-49852.appspot.com",
-    messagingSenderId: "248876370595",
-    appId: "1:248876370595:web:68a2557132def0ae38cd16",
-    measurementId: "G-BK0G4NM456"
-  };
+  apiKey: "AIzaSyDuVJBwLc6kIS8ri_OZLB6qrcV_DdK7pR0",
+  authDomain: "live-notes-49852.firebaseapp.com",
+  projectId: "live-notes-49852",
+  storageBucket: "live-notes-49852.appspot.com",
+  messagingSenderId: "248876370595",
+  appId: "1:248876370595:web:68a2557132def0ae38cd16",
+  measurementId: "G-BK0G4NM456",
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const firestore = firebase.firestore();
-  export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const auth = firebase.auth();
 
-  export const provider = new firebase.auth.GoogleAuthProvider();
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
-  export const signOut = () => auth.signOut();
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signOut = () => auth.signOut();
 
-  window.firebase = firebase;
+window.firebase = firebase;
 
-  export const createUserProfileDocument = async (user, additionalData) => {
-      if (!user) return;
+export const createUserProfileDocument = async (user, additionalData) => {
+  if (!user) return;
 
-      const userRef = firestore.doc(`users/${user.uid}`);
-      const snapshot = await userRef.get();
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
 
-      if (!snapshot.exists) {
-          const { displayName, email, photoURL } = user;
-          const createdAt = new Date();
-          try {
-              await userRef.set({
-                displayName,
-                email,
-                photoURL,
-                createdAt,
-                ...additionalData,
-              });
-          } catch(error) {
-              console.error('Error creating user profile', error);
-          }
-      }
-
-      return getUserDocument(user.uid);
+  if (!snapshot.exists) {
+    const { displayName, email, photoURL } = user;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        photoURL,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error("Error creating user profile", error);
+    }
   }
 
-  export const getUserDocument = async (uid) => {
-      if (!uid) return null;
+  return getUserDocument(user.uid);
+};
 
-      try {
-          const userDocument = await firestore.collection('users').doc(uid).get();
+export const getUserDocument = async (uid) => {
+  if (!uid) return null;
 
-          return { uid, ...userDocument.data() };
-      } catch(error) {
-          console.error('Error fetching user profule', error);
-      }
+  try {
+    return firestore.collection("users").doc(uid);
+  } catch (error) {
+    console.error("Error fetching user profile", error);
   }
+};
 
-  export default firebase;
+export default firebase;
