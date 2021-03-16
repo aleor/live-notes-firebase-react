cc00dd71c41824d668dc754292da647f47c43cc0
Rustem Akhmetov <rustem.akhmetov@sonicdrivein.com>
2021-03-16 12:20:57 +0100

    delete only own posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import moment from 'moment';
+import moment from "moment";
 
-import { firestore } from '../firebase';
+import { firestore } from "../firebase";
+import { UserContext } from "../providers/UserProvider";
+
+const belongsToCurrentUser = (currentUser, postAuthor) => {
+  if (!currentUser) return false;
+
+  return currentUser.uid === postAuthor.uid;
+};
 
 const Post = ({ title, content, user, createdAt, stars, comments, id }) => {
+  const currentUser = useContext(UserContext);
+
   const postRef = firestore.doc(`posts/${id}`);
   const remove = () => postRef.delete();
   const star = () => postRef.update({ stars: stars + 1 });
@@ -33,8 +42,14 @@ const Post = ({ title, content, user, createdAt, stars, comments, id }) => {
           <p>{moment(createdAt?.toDate()).calendar()}</p>
         </div>
         <div>
-          <button className="star" onClick={star}>Star</button>
-          <button className="delete" onClick={remove}>Delete</button>
+          <button className="star" onClick={star}>
+            Star
+          </button>
+          {belongsToCurrentUser(currentUser, user) && (
+            <button className="delete" onClick={remove}>
+              Delete
+            </button>
+          )}
         </div>
       </div>
     </article>
