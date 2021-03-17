492cfcfe06bfd51f4f7a1659ef562e43db5324dc
Rustem Akhmetov <rustem.akhmetov@sonicdrivein.com>
2021-03-17 13:48:48 +0100

    firebase functions to sanitize and handle updates

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,44 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+
+exports.getAllPosts = functions.https.onRequest(async (request, response) => {
+    const snapshot = await firestore.collection('posts').get();
+    const posts = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    response.json({ posts });
+});
+
+exports.sanitizeContent = functions.firestore
+    .document('posts/{postId}')
+    .onWrite(async (change) => {
+        if (!change.after.exists) return;
+
+        const { content, sanitized } = change.after.data();
+
+        if (content && !sanitized) {
+            return change.after.ref.update({
+                content: content.replace(/Sensitive/g, '****'),
+                sanitized: true,
+            });
+        }
+
+        return null;
+    }
+);
+
+exports.incrementCommentCount = functions.firestore
+    .document('/posts/{postId}/comments/{commentId}')
+    .onCreate(async (snapshot, context) => {
+        const { postId } = context.params;
+        const { postRef } = firestore.doc(`posts/${postId}`);
+
+        const snapshot = await postRef.get('comments');
+        const comments = snapshot.get('comments');
+
+        return postRef.update({ comments: comments + 1 });
+    }
+);
